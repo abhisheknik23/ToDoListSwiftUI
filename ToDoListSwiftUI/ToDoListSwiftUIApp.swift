66a29d538e23844c8c9e5b1fb45b7d94de004431
Abhishek Gupta <abhisheknik23@gmail.com>
2024-02-28 22:51:01 +0530

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Abhishek Gupta on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
